db52aea4bd8eba4c8e8ff520eaad32f49e5055d5
Owain Jones <owj3@aber.ac.uk>
2016-05-03 15:38:45 +0100

    Continued with JavaFX

--- Bonks and Zaps Assignment owj3/src/GameApplication.java
+++ Bonks and Zaps Assignment owj3/src/GameApplication.java
@@ -1,22 +1,6 @@
 import java.util.Scanner;
-import javafx.application.Application;
+
 import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 /**
  * Controls the menu system, settings and the initiation of the game and GameEngine.
@@ -25,11 +9,8 @@ import javafx.stage.Stage;
  * @version 1.0
  *
  */
-public class GameApplication extends Application {
+public class GameApplication {
 	
-	private Label[] labels = new Label[5];
-	private boolean doRun;
-
 	GameEngine gameEngine = new GameEngine();
 
 	private Scanner in;
@@ -55,106 +36,13 @@ public class GameApplication extends Application {
 	 * @throws CannotActException
 	 * @throws InterruptedException
 	 */
-	public static void main(String[] args) throws CannotActException, InterruptedException {
-		
-		launch(args);
-		//GameApplication app = new Application();
-		//app.callMenu();
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		Border border = new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1)));
-		BorderPane borderPane = new BorderPane();
-		borderPane.setPadding(new Insets(25,25,25,25)); 
-		stage.setTitle("*****CS12320 INDIVIDUAL ASSIGNMENT - OWAIN JONES - owj3@aber.ac.uk*****");
-		
-		
-		labels[2] = new Label(); //right
-		labels[2].setPrefSize(100, 700);
-		labels[2].setBorder(border);
-		
-		
-		labels[3] = new Label(); //left
-		labels[3].setPrefSize(100, 700);
-		labels[3].setBorder(border);
-	
-		borderPane.setRight(labels[2]);
-		borderPane.setLeft(labels[3]);
-		
-		///////Top Title Panel///////
-		VBox titlePanel = new VBox();
-		
-		Label title = new Label("***** BONKS AND ZAPS SIMULATION *****"); //top
-		title.setPrefSize(810, 20);
-		//labels[0].setBorder(border);
-		
-		Label subTitle = new Label("*****CS12320 INDIVIDUAL ASSIGNMENT - OWAIN JONES - owj3@aber.ac.uk*****"); //top
-		subTitle.setPrefSize(810, 20);
-		//labels[1].setBorder(border);
-		
-		titlePanel.getChildren().add(title);
-		titlePanel.getChildren().add(subTitle);
-		
-		title.setId("title");
-		subTitle.setId("title");
-		
-		borderPane.setTop(titlePanel);	
-		/////////////////////////////
-		
-		
-		/////Bottom Button Panel/////
-		HBox buttonPanel = new HBox();
-		Button startSimulation = new Button("Start Simulation");
-		startSimulation.setOnAction(e -> printMenu());
-		startSimulation.setPrefSize(120,50);
-		startSimulation.setId("btnlabel");
-		Button pauseSimulation = new Button("Pause Simulation");
-		pauseSimulation.setOnAction(e -> printMenu());
-		pauseSimulation.setPrefSize(120,50);
-		Button changeSettings = new Button("Change Settings");
-		changeSettings.setOnAction(e -> printMenu());
-		changeSettings.setPrefSize(120,50);
-		Button reset = new Button("Reset GridWorld to defaults");
-		reset.setOnAction(e -> printMenu());
-		reset.setPrefSize(170,50);
-		Button displaySettings = new Button("Display current settings");
-		displaySettings.setOnAction(e -> printMenu());
-		displaySettings.setPrefSize(160,50);
-		Button quit = new Button("Quit");
-		quit.setOnAction(e -> printMenu());
-		quit.setPrefSize(120,50);
-			
-		buttonPanel.getChildren().add(startSimulation);
-		buttonPanel.getChildren().add(pauseSimulation);
-		buttonPanel.getChildren().add(changeSettings);
-		buttonPanel.getChildren().add(reset);
-		buttonPanel.getChildren().add(displaySettings);
-		buttonPanel.getChildren().add(quit);
-		
-		borderPane.setBottom(buttonPanel);
-		/////////////////////////////
-		
-		Scene scene = new Scene(borderPane);
-		stage.setScene(scene);
-		
-		scene.getStylesheets().add(GameApplication.class.getResource("styling.css").toExternalForm());
-		
-		stage.setOnCloseRequest(e->{doRun = false; Platform.exit();});
-		
-		stage.setMaxWidth(1000);
-		stage.setMaxHeight(700);
-		
-		stage.show();	
-			
-		
-	}
 
 	/**
 	 * Constructor for Application that creates a new Scanner.
 	 */
 	public GameApplication() {
 		in = new Scanner(System.in);
+		defaultSettings();
 	}
 
 	/**
@@ -213,6 +101,50 @@ public class GameApplication extends Application {
 			}
 		} while (!choice.equals("Q"));
 	}
+	
+	public void guiMenuSelector(String choice) throws CannotActException, InterruptedException {
+
+			switch (choice) {
+
+			case "1":
+				createGridWorld();
+				return;
+
+			case "2":
+				try {
+					startGridWorld();
+				} catch (CannotActException | InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return;
+
+			case "3":
+				changeSettings();
+				return;
+
+			case "4":
+				resetSimulation();
+				return;
+
+
+			case "5":
+				printCurrentSettings();
+				return;
+
+
+			case "6":
+				printGridWorld();
+				return;
+
+			case "Q":
+				exitProgram();
+				return;
+
+			default:
+				System.err.println("==Not a valid choice==");
+			}  while (!choice.equals("Q"));
+	}
 
 	/**
 	 * Prints out the menu to the screen
@@ -234,7 +166,7 @@ public class GameApplication extends Application {
 	 * Also sets isGridWorldGenerated and hasBeingsSpawned to true to indicate that they
 	 * have been created.
 	 */
-	private void createGridWorld() {
+	public void createGridWorld() {
 		gameEngine.createGridworld(gridWorldX, gridWorldY);
 		isGridWorldGenerated = true;
 		
@@ -252,7 +184,7 @@ public class GameApplication extends Application {
 	 * @throws CannotActException
 	 * @throws InterruptedException
 	 */
-	private void startGridWorld() throws CannotActException, InterruptedException {
+	public void startGridWorld() throws CannotActException, InterruptedException {
 		if (isGridWorldGenerated == true && hasBeingsSpawned == true) {
 		gameEngine.setMaxDayCount(maxDayCount);
 		gameEngine.startSimulation();
@@ -268,7 +200,7 @@ public class GameApplication extends Application {
 	 * It prints out toString for GridWorld which shows the entire state of GridWorld and the toStrings of
 	 * all of it's inhabitants. (quite messy but useful for debugging).
 	 */
-	private void printGridWorld() {
+	public void printGridWorld() {
 		System.out.println("FOR DEBUG ONLY - PRINTING...");
 		if (isGridWorldGenerated == true) {
 			System.out.println(gameEngine.gridWorld.toString());
@@ -284,7 +216,7 @@ public class GameApplication extends Application {
 	 * Used as an aid to the user, so they know whether GridWorld has been created 
 	 * and populated.
 	 */
-	private void status() {
+	public void status() {
 		System.out.print("\n" + "GridWorld = ");
 		if (isGridWorldGenerated == true) {
 			System.out.print(" Generated ");
@@ -309,7 +241,7 @@ public class GameApplication extends Application {
 	 * "maxDayCount = 20;"
 	 * "settingsName = "Default";"
 	 */
-	private void defaultSettings() {
+	public void defaultSettings() {
 		bonkStartPopulation = 20;
 		zapStartPopulation = 5;
 		gridWorldX = 5; // Column
@@ -323,7 +255,7 @@ public class GameApplication extends Application {
 	 * It set's the GridWorld maximum X and Y sizes, the Bonk and Zap start populations,
 	 * and also the simulation length in days/cycles
 	 */
-	private void changeSettings() {
+	public void changeSettings() {
 		System.out.println("===Edit Settings===");
 		System.out.println("Please type Grid World X (column) value: ");
 		gridWorldX = in.nextInt(); // Column
@@ -344,7 +276,7 @@ public class GameApplication extends Application {
 	 * Prints the current max X and Y of GridWorld, the Bonk and Zap start populations,
 	 * and also the simulation length in days/cycles
 	 */
-	private void printCurrentSettings() {
+	public void printCurrentSettings() {
 		System.out.println("\n" + "Current Settings: " + settingsName + "\n" + "GridWorld Size: [" + gridWorldX + ", "
 				+ gridWorldY + "]" + "\n" + "Bonks start population: " + bonkStartPopulation + "\n"
 				+ "Zap start population: " + zapStartPopulation + "\n" + "GridWorld day count: " + maxDayCount);
@@ -354,7 +286,7 @@ public class GameApplication extends Application {
 	 * This resets the game to it's default settings by loading the defaults and resetting
 	 * GridWorld.
 	 */
-	private void resetSimulation() {
+	public void resetSimulation() {
 		defaultSettings();
 		System.out.println("===Default settings loaded===");
 		printCurrentSettings();
@@ -366,7 +298,7 @@ public class GameApplication extends Application {
 	/**
 	 * This method exit's the program.
 	 */
-	private void exitProgram() {
+	public void exitProgram() {
 		System.out.println("*** THANK YOU FOR USING BONKS AND ZAPS ***");
 	}
 
---
+++ Bonks and Zaps Assignment owj3/src/main.java
@@ -0,0 +1,181 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * This main class starts the program and also controls all of the GUI.
+ * If the program is launched with the "-t" argument then control is handed
+ * stright to the GameApplication class, which will then run a text only version
+ * of the game
+ * 
+ * @author Owain Jones
+ * @version 1.0
+ *
+ */
+public class main extends Application {
+
+	private Label[] labels = new Label[5];
+	private boolean doRun;
+	private Thread runGame;
+
+	GameApplication app = new GameApplication();
+
+	public static void main(String[] args) throws CannotActException, InterruptedException {
+
+		// String text = args[0];
+
+		// if (text == "-t") {
+		//GameApplication app = new GameApplication();
+		//app.callMenu();
+		// } else {
+		launch(args);
+		// }
+	}
+
+	public void start(Stage stage) throws CannotActException, InterruptedException, Exception {
+		Border border = new Border(
+				new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1)));
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(25, 25, 25, 25));
+
+		stage.setTitle("*****CS12320 INDIVIDUAL ASSIGNMENT - OWAIN JONES - owj3@aber.ac.uk*****");
+
+		labels[2] = new Label(); // right
+		labels[2].setPrefSize(100, 700);
+		labels[2].setBorder(border);
+
+		labels[3] = new Label(); // left
+		labels[3].setPrefSize(100, 700);
+		labels[3].setBorder(border);
+
+		borderPane.setRight(labels[2]);
+		borderPane.setLeft(labels[3]);
+
+		/////// Top Title Panel///////
+		VBox titlePanel = new VBox();
+		titlePanel.setAlignment(Pos.BASELINE_CENTER);
+		titlePanel.setPadding(new Insets(1, 25, 25, 25));
+
+		Label title = new Label("***** BONKS AND ZAPS SIMULATION *****"); // top
+		title.setPrefSize(810, 20);
+		title.setAlignment(Pos.BASELINE_CENTER);
+		title.setId("title");
+		//title.setBorder(border);
+
+		Label subTitle = new Label("*****CS12320 INDIVIDUAL ASSIGNMENT - OWAIN JONES - owj3@aber.ac.uk*****"); // top
+		subTitle.setPrefSize(810, 20);
+		subTitle.setAlignment(Pos.BASELINE_CENTER);
+		subTitle.setId("title");
+		//subTitle.setBorder(border);
+
+		titlePanel.getChildren().add(title);
+		titlePanel.getChildren().add(subTitle);
+
+		borderPane.setTop(titlePanel);
+		/////////////////////////////
+
+		/////Bottom Button Panel/////
+		HBox buttonPanel = new HBox();
+		Button startSimulation = new Button("Start Simulation");
+		startSimulation.setOnAction(e -> menu("2"));
+		startSimulation.setPrefSize(120, 50);
+		startSimulation.setId("btnlabel");
+		Button pauseSimulation = new Button("Pause Simulation");
+		pauseSimulation.setOnAction(e -> stopSimulation());
+		pauseSimulation.setPrefSize(120, 50);
+		Button createGridWorld = new Button("Create GridWorld");
+		createGridWorld.setOnAction(e -> menu("1"));
+		createGridWorld.setPrefSize(120, 50);
+		createGridWorld.setId("btnlabel");
+		Button changeSettings = new Button("Change Settings");
+		changeSettings.setOnAction(e -> menu("3"));
+		changeSettings.setPrefSize(120, 50);
+		Button reset = new Button("Reset GridWorld to defaults");
+		reset.setOnAction(e -> menu("4"));
+		reset.setPrefSize(170, 50);
+		Button displaySettings = new Button("Display current settings");
+		displaySettings.setOnAction(e -> menu("5"));
+		displaySettings.setPrefSize(160, 50);
+		Button quit = new Button("Quit");
+		quit.setOnAction(e -> menu("Q"));
+		quit.setPrefSize(120, 50);
+
+		buttonPanel.getChildren().add(startSimulation);
+		buttonPanel.getChildren().add(pauseSimulation);
+		buttonPanel.getChildren().add(createGridWorld);
+		buttonPanel.getChildren().add(changeSettings);
+		buttonPanel.getChildren().add(reset);
+		buttonPanel.getChildren().add(displaySettings);
+		buttonPanel.getChildren().add(quit);
+
+		borderPane.setBottom(buttonPanel);
+		/////////////////////////////
+		
+		/////Middle Console Panel/////
+		
+		//////////////////////////////
+
+		Scene scene = new Scene(borderPane);
+		
+		//scene.getStylesheets().add(GameApplication.class.getResource("styling.css").toExternalForm());
+		
+		stage.setScene(scene);
+		stage.setOnCloseRequest(e->{doRun = false; Platform.exit();});
+		
+		stage.setMaxWidth(1000);
+		stage.setMaxHeight(700);
+
+		stage.show();
+	}
+
+	private void stopSimulation() {
+		doRun = false;
+	}
+
+	private void menu(String choice) {
+
+		runGame = new Thread() {
+			public void run() {
+				doRun = true;
+
+				while (doRun) {
+					Platform.runLater(new Runnable() {
+						public void run() {
+							try {
+								app.guiMenuSelector(choice);
+								doRun = false;
+							} catch (CannotActException | InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					});
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+		};
+		runGame.start();
+	}
+
+}
